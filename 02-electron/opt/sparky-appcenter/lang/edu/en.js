0f3487fe79cbcec119b476f3c52d73564a858b13
pavroo <pavroo@onet.eu>
2020-07-10 18:48:07 +0200

    20200710

--- 02-electron/opt/sparky-appcenter/lang/edu/en.js
+++
@@ -1,7 +0,0 @@
-// English localization file for Education
-// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
-// Under the GNU GPL v3
-// Last update 2020/01/01 by pavroo
-dict.entries['edu'] = {};
-// desktops
-dict.entries['edu'][''] = "";
