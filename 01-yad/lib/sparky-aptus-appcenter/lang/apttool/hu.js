3f922fbea6762a1cfd0b6bfdd458c9d4f147b5c6
pavroo <pavroo@onet.eu>
2020-07-30 16:01:56 +0200

    20200730

---
+++ 01-yad/lib/sparky-aptus-appcenter/lang/apttool/hu.js
@@ -0,0 +1,25 @@
+// Hungarian localization file for APTus Tools (translated by nemomen)
+// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
+// Under the GNU GPL v3
+// Last update 2020/06/18 by pavroo
+dict.entries['apttool'] = {};
+// aptus tools
+dict.entries['apttool']['refresh'] = "Frissítse a csomaglistát";
+dict.entries['apttool']['upgrade'] = "Frissítse biztonságosan a rendszert";
+dict.entries['apttool']['fullupgrade'] = "Teljes rendszerfrissítés";
+dict.entries['apttool']['installdebrepo'] = "Telepítsen csomagot a tárolóból";
+dict.entries['apttool']['installdeblocal'] = "Telepítsen csomagot a helyi lemezről";
+dict.entries['apttool']['fixpackages'] = "Javítsa ki a törött csomagokat";
+dict.entries['apttool']['aptclean'] = "Tisztítsa ki az összes DEB gyorsítótár csomagot";
+dict.entries['apttool']['aptautoremove'] = "Automatikus régi csomag eltávolítás";
+dict.entries['apttool']['aptremove'] = "Távolítsa el a csomagot a rendszerből";
+dict.entries['apttool']['quicklist'] = "Keressen csomagokat a tárolókban";
+dict.entries['apttool']['mainrepo'] = "Javítsa a főtárolót";
+dict.entries['apttool']['customrepo'] = "Javítsa az egyéni (custom) tárolókat";
+dict.entries['apttool']['lang'] = "Telepítsen nyelvi csomagokat";
+dict.entries['apttool']['nonfree'] = "Távolítsa el a Non-Free csomagokat";
+dict.entries['apttool']['usercache'] = "Tisztítsa ki a felhasználó „home” könyvtára gyorsítótárát";
+dict.entries['apttool']['debkernel'] = "Telepítse a Debian kernelt";
+dict.entries['apttool']['liqkernel'] = "Telepítse a Liquorix kernelt";
+dict.entries['apttool']['spakernel'] = "Telepítse a Sparky kernelt";
+dict.entries['apttool']['remkernel'] = "Távolítsa el a régi/korábbi rendszer-kernelt";
