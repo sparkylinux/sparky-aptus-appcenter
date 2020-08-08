bd8d4f6523f36379e1b6e7cc7218bf337bb244cf
pavroo <pavroo@onet.eu>
2020-08-08 23:22:48 +0200

    20200808

---
+++ 01-yad/lib/sparky-aptus-appcenter/lang/main/ru.js
@@ -0,0 +1,119 @@
+// Russian localization file for main
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2020/07/20 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['applications'] = "Приложения";
+dict.entries['main']['categories'] = "Категории";
+dict.entries['main']['aptus'] = "APTus";
+dict.entries['main']['info'] = "Информация";
+dict.entries['main']['translation'] = "Переводы:";
+// categories
+dict.entries['main']['accessories'] = "Инструменты";
+dict.entries['main']['desktops'] = "Рабочие столы";
+dict.entries['main']['development'] = "Разработка";
+dict.entries['main']['education'] = "Образование";
+dict.entries['main']['edu'] = "Edu";
+dict.entries['main']['games'] = "Игры";
+dict.entries['main']['graphics'] = "Графика";
+dict.entries['main']['internet'] = "Интернет";
+dict.entries['main']['multimedia'] = "Мультимедиа";
+dict.entries['main']['office'] = "Офис";
+dict.entries['main']['science'] = "Наука";
+dict.entries['main']['security'] = "Безопасность";
+dict.entries['main']['system'] = "Система";
+// info
+dict.entries['main']['info01'] = "Sparky APTus AppCenter позволяет выполнять множество заданий над пакетами в графическом режиме, например:";
+dict.entries['main']['info02'] = "обновление пакетов";
+dict.entries['main']['info03'] = "установка популярных приложений";
+dict.entries['main']['info04'] = "удаление пакетов";
+dict.entries['main']['info05'] = "и многое другое...";
+dict.entries['main']['info06'] = "Приложение использует YAD и HTML технологии для работы.";
+dict.entries['main']['info07'] = "YAD должен быть скомпилирован с флагом --html, включенным по умолчанию.";
+dict.entries['main']['info08'] = "Если вы обнаружите какие-либо проблемы, сообщите об этом на форумах Sparky, пожалуйста.";
+dict.entries['main']['info09'] = "Sparky APTus AppCenter выпущен под лицензией GNU GPL 3.0.";
+dict.entries['main']['info10'] = "Авторы:";
+dict.entries['main']['info11'] = "Переводы (на основе APTus):";
+// accesories
+dict.entries['main']['archivers'] = "Архивы";
+dict.entries['main']['clipboards'] = "Буферы обмена";
+dict.entries['main']['devices'] = "Устройства";
+dict.entries['main']['display'] = "Менеджеры дисплея";
+dict.entries['main']['filemanagers'] = "Файловые менеджеры";
+dict.entries['main']['monitor'] = "Мониторинг";
+dict.entries['main']['term'] = "Терминалы";
+dict.entries['main']['usb'] = "USB";
+// games
+dict.entries['main']['adventure'] = "Приключение";
+dict.entries['main']['arcade'] = "Аркады";
+dict.entries['main']['board'] = "Настольные";
+dict.entries['main']['cards'] = "Карты";
+dict.entries['main']['gameedu'] = "Образовательные";
+dict.entries['main']['fps'] = "FPS";
+dict.entries['main']['platform'] = "Платформеры";
+dict.entries['main']['puzzle'] = "Пазлы";
+dict.entries['main']['rpg'] = "RPG";
+dict.entries['main']['simulation'] = "Симуляторы";
+dict.entries['main']['sport'] = "Спорт";
+dict.entries['main']['strategy'] = "Стратегия";
+dict.entries['main']['text'] = "Текст";
+dict.entries['main']['emulators'] = "Эмуляторы";
+dict.entries['main']['tools'] = "Инструменты";
+dict.entries['main']['winapi'] = "Win API";
+// graphics
+dict.entries['main']['graphicseditors'] = "Графические редакторы";
+dict.entries['main']['graphicsviewers'] = "Просмотрщики графики";
+dict.entries['main']['graphicsscreenshots'] = "Снимки экрана";
+// internet
+dict.entries['main']['communication'] = "Связь";
+dict.entries['main']['email'] = "eMail";
+dict.entries['main']['filetransfer'] = "Передача файлов";
+dict.entries['main']['p2p'] = "p2p";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['universalaccess'] = "Универсальный доступ";
+dict.entries['main']['webbrowsers'] = "Web Браузеры";
+// multimedia
+dict.entries['main']['audioeditors'] = "Аудио редакторы";
+dict.entries['main']['audioplayers'] = "Аудио проигрыватели";
+dict.entries['main']['audiotools'] = "Аудио инструменты";
+dict.entries['main']['burn'] = "Запись CD/DVD";
+dict.entries['main']['codecs'] = "Кодеки";
+dict.entries['main']['mixers'] = "Миксеры";
+dict.entries['main']['subtitles'] = "Субтитры";
+dict.entries['main']['videoeditors'] = "Видео инструменты";
+dict.entries['main']['videoplayers'] = "Видео проигрыватели";
+// office
+dict.entries['main']['addressbooks'] = "Адресные книги";
+dict.entries['main']['calculators'] = "Калькуляторы";
+dict.entries['main']['documentviewers'] = "Просмотр документов";
+dict.entries['main']['finance'] = "Финансы";
+dict.entries['main']['notes'] = "Заметки";
+dict.entries['main']['officesuite'] = "Офисный пакеты";
+dict.entries['main']['spreadsheet'] = "Электронные таблицы";
+dict.entries['main']['texteditors'] = "Текстовые редакторы";
+// others
+dict.entries['main']['others'] = "Другие";
+dict.entries['main']['next'] = "Следующая страница";
+dict.entries['main']['back'] = "Предыдущая страница";
+// system
+dict.entries['main']['backup'] = "Резервное копирование";
+dict.entries['main']['disk'] = "Диск";
+dict.entries['main']['encrypt'] = "Шифрование";
+dict.entries['main']['filesystems'] = "Файловые системы";
+dict.entries['main']['kernel'] = "Ядра";
+dict.entries['main']['package'] = "Пакеты";
+dict.entries['main']['shells'] = "Терминалы";
+dict.entries['main']['virtual'] = "Virtual";
+// search
+dict.entries['main']['search'] = "Поиск";
+dict.entries['main']['info12'] = "Результат поиска по слову:";
+dict.entries['main']['info13'] = "Извините, не могу найти то, что вы ищете.";
+dict.entries['main']['info14'] = "Обновите список пакетов и попробуйте снова, используя более конкретное имя пакета.";
+dict.entries['main']['info15'] = "Вернуться к:";
+dict.entries['main']['info16'] = "Главная страница.";
+dict.entries['main']['info17'] = "Результат поиска по слову:";
+dict.entries['main']['info18'] = "Существует более одного из ваших результатов поиска.";
+dict.entries['main']['info19'] = "Пожалуйста, выберите одну страницу, которая лучше подходит для вашего ключевого слова.";
+dict.entries['main']['info20'] = "больше чем один.";
+dict.entries['main']['info21'] = "NULL";
