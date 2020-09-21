170b022653111263634e78ccab1cdf67025516d7
EltonFF <eltonfabricio10@gmail.com>
2020-09-21 00:43:21 -0300

    pt_BR translation

---
+++ 01-yad/lib/sparky-aptus-appcenter/lang/apttool/pt_BR.js
@@ -0,0 +1,25 @@
+// Portuguese-Brazil localization file for APTus Tools by eltonff
+// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
+// Under the GNU GPL v3
+// Last update 2020/06/18 by pavroo
+dict.entries['apttool'] = {};
+// aptus tools
+dict.entries['apttool']['refresh'] = "Atualizar lista de pacotes";
+dict.entries['apttool']['upgrade'] = "Atualizar sistema com segurança";
+dict.entries['apttool']['fullupgrade'] = "Atualização completa do sistema";
+dict.entries['apttool']['installdebrepo'] = "Instalar pacotes do repositório";
+dict.entries['apttool']['installdeblocal'] = "Instalar pacote da unidade local";
+dict.entries['apttool']['fixpackages'] = "Corrigir pacotes quebrados";
+dict.entries['apttool']['aptclean'] = "Limpar todos os pacotes DEB em cache";
+dict.entries['apttool']['aptautoremove'] = "Remover pacotes antigos automaticamente";
+dict.entries['apttool']['aptremove'] = "Remover pacote do sistema";
+dict.entries['apttool']['quicklist'] = "Pesquisar pacotes do repositório";
+dict.entries['apttool']['mainrepo'] = "Editar repositório principal";
+dict.entries['apttool']['customrepo'] = "Editar repositórios personalizados";
+dict.entries['apttool']['lang'] = "Instalar pacotes de idiomas";
+dict.entries['apttool']['nonfree'] = "Remover pacotes não livres";
+dict.entries['apttool']['usercache'] = "Limpar o cache do diretório principal do usuário";
+dict.entries['apttool']['debkernel'] = "Instalar kernel Debian";
+dict.entries['apttool']['liqkernel'] = "Instalar kernel Liquorix";
+dict.entries['apttool']['spakernel'] = "Instalar kernel Sparky";
+dict.entries['apttool']['remkernel'] = "Remover kernel antigo do sistema";
