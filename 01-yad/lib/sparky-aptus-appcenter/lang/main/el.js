af5a1cde0c5bfed4349dd151e0bad5e668fa624b
pavroo <pavroo@onet.eu>
2021-04-01 21:44:19 +0200

    20210401

---
+++ 01-yad/lib/sparky-aptus-appcenter/lang/main/el.js
@@ -0,0 +1,121 @@
+// Greek localization file for main
+// Originally created by Piotr "MoroS" MroÅ¼ek 2018
+// Under the GNU GPL v3
+// Last update 2021/02/28 by jidan
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['applications'] = "Εφαρμογές";
+dict.entries['main']['categories'] = "Κατηγορίες";
+dict.entries['main']['aptus'] = "APTus";
+dict.entries['main']['info'] = "Πληροφορίες";
+dict.entries['main']['translation'] = "Μεταφράσεις:";
+// categories
+dict.entries['main']['accessories'] = "Βοηθήματα";
+dict.entries['main']['desktops'] = "Περιβάλλοντα Εργασίας";
+dict.entries['main']['development'] = "Προγραμματισμός";
+dict.entries['main']['education'] = "Εκπαίδευση";
+dict.entries['main']['edu'] = "Εκπ.";
+dict.entries['main']['games'] = "Παιχνίδια";
+dict.entries['main']['graphics'] = "Γραφικά";
+dict.entries['main']['internet'] = "Δίκτυο";
+dict.entries['main']['multimedia'] = "Πολυμέσα";
+dict.entries['main']['office'] = "Γραφείο";
+dict.entries['main']['science'] = "Επιστήμη";
+dict.entries['main']['security'] = "Ασφάλεια";
+dict.entries['main']['system'] = "Σύστημα";
+// info
+dict.entries['main']['info01'] = "Το Sparky APTus AppCenter διεκπεραιώνει πολλές εργασίες διαχείρισης πακέτων μέσα από ένα γραφικό περιβάλλον, όπως:";
+dict.entries['main']['info02'] = "ενημέρωση πακέτων";
+dict.entries['main']['info03'] = "εγκατάσταση δημοφιλών εφαρμογών";
+dict.entries['main']['info04'] = "απεγκατάσταση πακέτων";
+dict.entries['main']['info05'] = "και πολλά ακόμα...";
+dict.entries['main']['info06'] = "Η εφαρμογή χρησιμοποιεί τεχνικές YAD και HTML.";
+dict.entries['main']['info07'] = "το YAD θα πρέπει να έχει μεταγλωττιστεί με την παράμετρο --html ως προεπιλογή.";
+dict.entries['main']['info08'] = "Αν εντοπίσετε κάποιο πρόβλημα, παρακαλείσθε να το αναφέρετε στο φόρουμ του Sparky.";
+dict.entries['main']['info09'] = "Το Sparky APTus AppCenter υπόκειται στην άδεια GNU GPL 3.0.";
+dict.entries['main']['info10'] = "Συνεισέφεραν:";
+dict.entries['main']['info11'] = "Μεταφράσεις (με βάση το APTus):";
+// accesories
+dict.entries['main']['archivers'] = "Πακετάρισμα";
+dict.entries['main']['clipboards'] = "Πρόχειρα";
+dict.entries['main']['devices'] = "Συσκευές";
+dict.entries['main']['display'] = "Διαχειριστές Εμφάνισης";
+dict.entries['main']['filemanagers'] = "Διαχειριστές αρχείων";
+dict.entries['main']['monitor'] = "Παρακολούθηση";
+dict.entries['main']['term'] = "Τερματικά";
+dict.entries['main']['usb'] = "USB";
+dict.entries['main']['weather'] = "Καιρός";
+// games
+dict.entries['main']['adventure'] = "Περιπέτειας";
+dict.entries['main']['arcade'] = "Ηλεκτρονικά (Arcade)";
+dict.entries['main']['board'] = "Επιτραπέζια";
+dict.entries['main']['cards'] = "Με Κάρτες";
+dict.entries['main']['gameedu'] = "Εκπαιδευτικά";
+dict.entries['main']['fps'] = "Με Όπλα (FPS)";
+dict.entries['main']['platform'] = "Πλατφόρμες";
+dict.entries['main']['puzzle'] = "Παζλ";
+dict.entries['main']['rpg'] = "Παιχνίδια Ρόλων (RPG)";
+dict.entries['main']['simulation'] = "Εξομοιωτές";
+dict.entries['main']['sport'] = "Σπορ";
+dict.entries['main']['strategy'] = "Στρατηγικής";
+dict.entries['main']['text'] = "Κειμένου";
+dict.entries['main']['emulators'] = "Εξομοιωτές Συστημάτων";
+dict.entries['main']['tools'] = "Εργαλεία";
+dict.entries['main']['winapi'] = "Εξομοιωτές Windows";
+// graphics
+dict.entries['main']['graphicseditors'] = "Επεξεργαστές Εικόνας";
+dict.entries['main']['graphicsviewers'] = "Εμφάνιση Εικόνας";
+dict.entries['main']['graphicsscreenshots'] = "Φωτογραφία Επιφάνειας Εργασίας";
+// internet
+dict.entries['main']['communication'] = "Επικοινωνία";
+dict.entries['main']['email'] = "Ηλεκτρονικό Ταχυδρομείο";
+dict.entries['main']['filetransfer'] = "Μεταφορά αρχείων";
+dict.entries['main']['p2p'] = "Δίκτυα χρήστης-προς-χρήστη (p2p)";
+dict.entries['main']['rss'] = "Ροές Ειδήσεων (RSS)";
+dict.entries['main']['universalaccess'] = "Βοηθήματα Πρόσβασης";
+dict.entries['main']['webbrowsers'] = "Περιηγητές διαδικτύου";
+// multimedia
+dict.entries['main']['audioeditors'] = "Επεξεργαστές Ήχου";
+dict.entries['main']['audioplayers'] = "Αναπαραγωγή Ήχου";
+dict.entries['main']['audiotools'] = "Εργαλεία Ήχου";
+dict.entries['main']['burn'] = "Δημιουργία CD/DVD";
+dict.entries['main']['codecs'] = "Κωδικοποιητές";
+dict.entries['main']['mixers'] = "Μείκτες";
+dict.entries['main']['subtitles'] = "Υπότιτλοι";
+dict.entries['main']['videoeditors'] = "Εργαλεία Βίντεο";
+dict.entries['main']['videoplayers'] = "Αναπαραγωγή Βίντεο";
+// office
+dict.entries['main']['addressbooks'] = "Βιβλία Διευθύνσεων";
+dict.entries['main']['calculators'] = "Αριθμομηχανές";
+dict.entries['main']['documentviewers'] = "Εμφάνιση Εγγράφων";
+dict.entries['main']['finance'] = "Οικονομικά";
+dict.entries['main']['notes'] = "Σημειώσεις";
+dict.entries['main']['officesuite'] = "Σουίτες Γραφείου";
+dict.entries['main']['spreadsheet'] = "Υπολογιστικά Φύλλα";
+dict.entries['main']['texteditors'] = "Επεξεργαστές Κειμένου";
+// others
+dict.entries['main']['others'] = "Άλλα";
+dict.entries['main']['next'] = "Επόμενη σελίδα";
+dict.entries['main']['back'] = "Τελευταία σελίδα";
+// system
+dict.entries['main']['backup'] = "Αντίγραφα Ασφαλείας";
+dict.entries['main']['disk'] = "Δίσκοι";
+dict.entries['main']['encrypt'] = "Κρυπτογράφηση";
+dict.entries['main']['filesystems'] = "Συστήματα Αρχείων";
+dict.entries['main']['kernel'] = "Πυρήνες";
+dict.entries['main']['package'] = "Πακέτα";
+dict.entries['main']['shells'] = "Shells";
+dict.entries['main']['virtual'] = "Εξομοιωτές";
+// search
+dict.entries['main']['search'] = "Αναζήτηση";
+dict.entries['main']['info12'] = "Αποτελέσματα αναζήτησης της λέξης:";
+dict.entries['main']['info13'] = "ΔΕΝ μπορεί να βρεθεί αυτό που αναζητάτε.";
+dict.entries['main']['info14'] = "Ανανεώστε τη λίστα πακέτων και δοκιμάστε ξανά με ένα πιο ακριβές όνομα.";
+dict.entries['main']['info15'] = "Πίσω στο:";
+dict.entries['main']['info16'] = "Η Κύρια σελίδα.";
+dict.entries['main']['info17'] = "Αποτελέσματα αναζήτησης της λέξης:";
+dict.entries['main']['info18'] = "Υπάρχουν περισσότερα από ένα αποτελέσματα.";
+dict.entries['main']['info19'] = "Επιλέξτε τη σελίδα που ταιριάζει καλύτερα στο πληκτρολόγιό σας.";
+dict.entries['main']['info20'] = "είναι πάνω από ένα.";
+dict.entries['main']['info21'] = "ΚΕΝΟ";
+
