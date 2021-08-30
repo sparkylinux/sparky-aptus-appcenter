15cadd7d54ced8a5e85b218c9335ad77100a024d
OkayPJ <xingjiapeng10@163.com>
2021-08-30 09:25:52 +0800

    Add zh_CN translation
    * Add zh_CN translation
    * Edit lib/sparky-aptus-appcenter/aptus-appcenter to check zh_CN in LANG var
    * Change my nickname to the commonly used one

---
+++ 7/lib/sparky-aptus-appcenter/lang/apttool/zh_CN.js
@@ -0,0 +1,26 @@
+// Chinese(Simplified) localization file for APTus Tools
+// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
+// Under the GNU GPL v3
+// Last update 2021/08/30 by OkayPJ <okaythepj@outlook.com>
+dict.entries['apttool'] = {};
+// aptus tools
+dict.entries['apttool']['refresh'] = "刷新软件包列表";
+dict.entries['apttool']['upgrade'] = "安全地升级系统";
+dict.entries['apttool']['fullupgrade'] = "完全升级系统";
+dict.entries['apttool']['installdebrepo'] = "从软件仓库中安装软件包";
+dict.entries['apttool']['installdeblocal'] = "在本地磁盘上安装软件包";
+dict.entries['apttool']['fixpackages'] = "修复破损软件包";
+dict.entries['apttool']['aptclean'] = "清除所有 DEB 的缓存软件包";
+dict.entries['apttool']['aptautoremove'] = "自动卸载旧的不需要的软件包";
+dict.entries['apttool']['aptremove'] = "从系统中卸载软件包";
+dict.entries['apttool']['quicklist'] = "在软件仓库中搜索软件包";
+dict.entries['apttool']['mainrepo'] = "编辑主软件源";
+dict.entries['apttool']['customrepo'] = "编辑自定义软件源列表";
+dict.entries['apttool']['lang'] = "安装语言包";
+dict.entries['apttool']['nonfree'] = "卸载非自由软件包";
+dict.entries['apttool']['usercache'] = "清除用户家目录缓存";
+dict.entries['apttool']['debkernel'] = "安装 Debian 内核";
+dict.entries['apttool']['liqkernel'] = "安装 Liquorix 内核";
+dict.entries['apttool']['spakernel'] = "安装 Sparky 内核";
+dict.entries['apttool']['xankernel'] = "安装 XanMod 内核";
+dict.entries['apttool']['remkernel'] = "卸载旧的系统内核";
