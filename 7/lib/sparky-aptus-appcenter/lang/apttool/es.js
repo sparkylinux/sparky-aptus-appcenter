ac93d638aa8b71c4c20dfc5c576a356b069fc907
pavroo <pavroo@onet.eu>
2022-07-04 22:12:42 +0200

    20220704

---
+++ 7/lib/sparky-aptus-appcenter/lang/apttool/es.js
@@ -0,0 +1,27 @@
+// Spanish (es_ES) localization file for APTus Tools
+// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
+// Under the GNU GPL v3
+// Last update 2020/10/20 by pavroo
+// Translated by depaz
+dict.entries['apttool'] = {};
+// aptus tools
+dict.entries['apttool']['refresh'] = "Actualizar la lista de paquetes";
+dict.entries['apttool']['upgrade'] = "Actualizar el sistema de forma segura";
+dict.entries['apttool']['fullupgrade'] = "Actualizar completamente el sistema";
+dict.entries['apttool']['installdebrepo'] = "Instalar paquetes del repositorio";
+dict.entries['apttool']['installdeblocal'] = "Instalar paquetes de un disco local";
+dict.entries['apttool']['fixpackages'] = "Reparar paquetes rotos";
+dict.entries['apttool']['aptclean'] = "Limpiar todos los paquetes DEB en caché";
+dict.entries['apttool']['aptautoremove'] = "Autoeliminar paquetes antiguos";
+dict.entries['apttool']['aptremove'] = "Eliminar paquete del sistema";
+dict.entries['apttool']['quicklist'] = "Buscar paquetes en el repositorio";
+dict.entries['apttool']['mainrepo'] = "Editar el repositorio principal";
+dict.entries['apttool']['customrepo'] = "Editar los repositorios personalizados";
+dict.entries['apttool']['lang'] = "Instalar paquetes de idiomas";
+dict.entries['apttool']['nonfree'] = "Eliminar paquetes no libres";
+dict.entries['apttool']['usercache'] = "Eliminar la caché de la home del usuario";
+dict.entries['apttool']['debkernel'] = "Instalar kernel Debian";
+dict.entries['apttool']['liqkernel'] = "Instalar kernel Liquorix";
+dict.entries['apttool']['spakernel'] = "Instalar kernel Sparky";
+dict.entries['apttool']['xankernel'] = "Instalar kernel XanMod";
+dict.entries['apttool']['remkernel'] = "Eliminar kernel antigua del sistema";
