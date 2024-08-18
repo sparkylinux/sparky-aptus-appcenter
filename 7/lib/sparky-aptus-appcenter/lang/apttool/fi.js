1bc9f65e83e2b46bd5bbd8cd838512a4259becc8
pavroo <pavroo@onet.eu>
2024-08-18 11:40:01 +0200

    20240810

---
+++ 7/lib/sparky-aptus-appcenter/lang/apttool/fi.js
@@ -0,0 +1,26 @@
+// APTus Toolsin suomenkielinen lokalisointitiedosto
+// Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
+// GNU GPL v3:n alla
+// Viimeisin päivitys 2020/10/20 pavroo
+dict.entries['aptool'] = {};
+// aptus-työkalut
+dict.entries['apttool']['refresh'] = "Päivitä pakettiluettelo";
+dict.entries['apttool']['upgrade'] = "Päivitä järjestelmä turvallisesti";
+dict.entries['aptool']['fullupgrade'] = "Täysi järjestelmäpäivitys";
+dict.entries['apttool']['installdebrepo'] = "Asenna paketit arkistosta";
+dict.entries['apttool']['installdeblocal'] = "Asenna paketti paikalliselta asemalta";
+dict.entries['apttool']['fixpackages'] = "Korjaa rikkinäiset paketit";
+dict.entries['apttool']['aptclean'] = "Puhdista kaikki DEB-välimuistissa olevat paketit";
+dict.entries['apttool']['aptautoremove'] = "Poista vanhat paketit automaattisesti";
+dict.entries['apttool']['aptremove'] = "Poista paketti järjestelmästä";
+dict.entries['aptool']['quicklist'] = "Hae paketteja arkistosta";
+dict.entries['aptool']['mainrepo'] = "Muokkaa päävarastoa";
+dict.entries['aptool']['customrepo'] = "Muokkaa mukautettuja tietovarastoja";
+dict.entries['aptool']['lang'] = "Asenna kielipaketit";
+dict.entries['aptool']['nonfree'] = "Poista ei-ilmaiset paketit";
+dict.entries['aptool']['usercache'] = "Puhdista käyttäjän kotihakemisto";
+dict.entries['aptool']['debkernel'] = "Asenna Debian-ydin";
+dict.entries['apttool']['liqkernel'] = "Asenna Liquorix-ydin";
+dict.entries['apttool']['spakernel'] = "Asenna Sparky-ydin";
+dict.entries['apttool']['xankernel'] = "Asenna XanMod-ydin";
+dict.entries['aptool']['remkernel'] = "Poista vanha järjestelmäydin";
