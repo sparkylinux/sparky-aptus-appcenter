a18880fa0b662758e959f5b74c896e4d291c6cd5
pavroo <pavroo@onet.eu>
2025-06-02 19:27:54 +0200

    20250602

---
+++ 8/lib/sparky-aptus-appcenter/lang/main/de.js
@@ -0,0 +1,126 @@
+// German localization file for main (29.05.2025)
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2024/03/04 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['applications'] = "Applikationen";
+dict.entries['main']['categories'] = "Kategorien";
+dict.entries['main']['aptus'] = "APTus";
+dict.entries['main']['info'] = "Info";
+dict.entries['main']['translation'] = "Übersetzungen:";
+// categories
+dict.entries['main']['accessories'] = "Zubehör";
+dict.entries['main']['desktops'] = "Desktops";
+dict.entries['main']['development'] = "Entwicklung";
+dict.entries['main']['education'] = "Bildung";
+dict.entries['main']['edu'] = "Bildun";
+dict.entries['main']['games'] = "Games";
+dict.entries['main']['graphics'] = "Grafik";
+dict.entries['main']['internet'] = "Internet";
+dict.entries['main']['multimedia'] = "Multimedia";
+dict.entries['main']['office'] = "Büro";
+dict.entries['main']['science'] = "Wissenschaft";
+dict.entries['main']['security'] = "Sicherheit";
+dict.entries['main']['system'] = "System";
+// info
+dict.entries['main']['info01'] = "Sparky APTus AppCenter ermöglicht Ihnen die Durchführung vieler Jobs an Paketen in einem grafischen Modus, wie etwa:";
+dict.entries['main']['info02'] = "Aktualisierung von Paketen";
+dict.entries['main']['info03'] = "Installation populärer Applikationen";
+dict.entries['main']['info04'] = "Entfernen von Paketen";
+dict.entries['main']['info05'] = "und vieles mehr ...";
+dict.entries['main']['info06'] = "Die Applikation arbeitet mit der YAD- und HTML-Technologie.";
+dict.entries['main']['info07'] = "YAD muss mit --html Flag kompiliert werden, was als Default aktiviert ist.";
+dict.entries['main']['info08'] = "Wenn Sie irgendein Problem finden, melden Sie es bitte im Sparky-Forum.";
+dict.entries['main']['info09'] = "Sparky APTus AppCenter wird unter der GNU GPL 3.0 License veröffentlicht.";
+dict.entries['main']['info10'] = "Mitwirkende:";
+dict.entries['main']['info11'] = "Übersetzungen (basierend auf APTus):";
+// accesories
+dict.entries['main']['archivers'] = "Archivierer";
+dict.entries['main']['chatbots'] = "Chatbots";
+dict.entries['main']['clipboards'] = "Zwischenablage";
+dict.entries['main']['devices'] = "Geräte";
+dict.entries['main']['display'] = "Display-Manager";
+dict.entries['main']['icons'] = "Icons";
+dict.entries['main']['filemanagers'] = "Dateimanager";
+dict.entries['main']['monitor'] = "Überwachung";
+dict.entries['main']['term'] = "Terminals";
+dict.entries['main']['themes'] = "Themes";
+dict.entries['main']['translators'] = "Übersetzungsprogramme";
+dict.entries['main']['usb'] = "USB";
+dict.entries['main']['weather'] = "Wettter";
+// games
+dict.entries['main']['adventure'] = "Abenteuer";
+dict.entries['main']['arcade'] = "Spielhalle";
+dict.entries['main']['board'] = "Brett";
+dict.entries['main']['cards'] = "Karten";
+dict.entries['main']['gameedu'] = "Pädagogisch";
+dict.entries['main']['fps'] = "FPS";
+dict.entries['main']['platform'] = "Plattform";
+dict.entries['main']['puzzle'] = "Puzzle";
+dict.entries['main']['rpg'] = "RPG";
+dict.entries['main']['simulation'] = "Simulation";
+dict.entries['main']['sport'] = "Sport";
+dict.entries['main']['strategy'] = "Strategie";
+dict.entries['main']['text'] = "Text";
+dict.entries['main']['emulators'] = "Emulatoren";
+dict.entries['main']['tools'] = "Tools";
+dict.entries['main']['winapi'] = "Win API";
+// graphics
+dict.entries['main']['graphicseditors'] = "Grafikeditoren";
+dict.entries['main']['graphicsviewers'] = "Grafikbetrachter";
+dict.entries['main']['graphicsscreenshots'] = "Screenshot";
+// internet
+dict.entries['main']['communication'] = "Kommunikation";
+dict.entries['main']['email'] = "E-Mail";
+dict.entries['main']['filetransfer'] = "Dateitransfer";
+dict.entries['main']['p2p'] = "p2p";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['universalaccess'] = "Universeller Zugang";
+dict.entries['main']['vpn'] = "VPN";
+dict.entries['main']['webbrowsers'] = "Webbrowser";
+// multimedia
+dict.entries['main']['audioeditors'] = "Audioeditoren";
+dict.entries['main']['audioplayers'] = "Audio-Player";
+dict.entries['main']['audiotools'] = "Audio-Tools";
+dict.entries['main']['burn'] = "CD/DVD brennen";
+dict.entries['main']['codecs'] = "Codecs";
+dict.entries['main']['mixers'] = "Mixer";
+dict.entries['main']['subtitles'] = "Untertitel";
+dict.entries['main']['videoeditors'] = "Video-Tools";
+dict.entries['main']['videoplayers'] = "Video-Player";
+// office
+dict.entries['main']['addressbooks'] = "Adressbücher";
+dict.entries['main']['calculators'] = "Rechner";
+dict.entries['main']['documentviewers'] = "Dokumentbetrachter";
+dict.entries['main']['finance'] = "Finanzen";
+dict.entries['main']['notes'] = "Notizen";
+dict.entries['main']['officesuite'] = "Office-Paket";
+dict.entries['main']['spreadsheet'] = "Tabellenkalkulationen";
+dict.entries['main']['texteditors'] = "Texteditoren";
+// others
+dict.entries['main']['others'] = "Sonstiges";
+dict.entries['main']['next'] = "Nächste Seite";
+dict.entries['main']['back'] = "Letzte Seite";
+dict.entries['main']['exit'] = "Beenden";
+// system
+dict.entries['main']['backup'] = "Backup";
+dict.entries['main']['disk'] = "Platte";
+dict.entries['main']['encrypt'] = "Verschlüsseln";
+dict.entries['main']['filesystems'] = "Dateisysteme";
+dict.entries['main']['kernel'] = "Kernel";
+dict.entries['main']['package'] = "Pakete (Packages)";
+dict.entries['main']['shells'] = "Shells";
+dict.entries['main']['virtual'] = "Virtuell";
+// search
+dict.entries['main']['search'] = "Suche";
+dict.entries['main']['info12'] = "Suchergebnis für das Wort";
+dict.entries['main']['info13'] = "Sorry, kann leider NICHT finden, wonach Sie suchen.";
+dict.entries['main']['info14'] = "Frischen Sie die Paketliste auf und versuchen Sie es erneut unter Verwendung eines spezifischeren Paketnamens.";
+dict.entries['main']['info15'] = "Zurück zu:";
+dict.entries['main']['info16'] = "Der Hauptseite.";
+dict.entries['main']['info17'] = "Suchergebnis für das Wort:";
+dict.entries['main']['info18'] = "Es gibt mehr als eines Ihrer Suchergebnisse.";
+dict.entries['main']['info19'] = "Bitte wählen Sie eine Seite, welche besser zu Ihrem Schlüsselwort passt.";
+dict.entries['main']['info20'] = "ist mehr als eine.";
+dict.entries['main']['info21'] = "NULL";
