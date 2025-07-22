32e5f96e635b273432f266fb2b1511b5f75abcf1
pavroo <pavroo@onet.eu>
2025-07-22 12:34:51 +0200

    20250722

--- 7/lib/sparky-aptus-appcenter/lang/main/de.js
+++ 7/lib/sparky-aptus-appcenter/lang/main/de.js
@@ -1,4 +1,4 @@
-// German localization file for main (29.05.2025)
+// German localization file for main (07.06.2025)
 // Originally created by Piotr "MoroS" Mrożek 2018
 // Under the GNU GPL v3
 // Last update 2024/03/04 by pavroo
@@ -14,7 +14,7 @@ dict.entries['main']['accessories'] = "Zubehör";
 dict.entries['main']['desktops'] = "Desktops";
 dict.entries['main']['development'] = "Entwicklung";
 dict.entries['main']['education'] = "Bildung";
-dict.entries['main']['edu'] = "Bildun";
+dict.entries['main']['edu'] = "Bildg.";
 dict.entries['main']['games'] = "Games";
 dict.entries['main']['graphics'] = "Grafik";
 dict.entries['main']['internet'] = "Internet";
@@ -38,7 +38,7 @@ dict.entries['main']['info11'] = "Übersetzungen (basierend auf APTus):";
 // accesories
 dict.entries['main']['archivers'] = "Archivierer";
 dict.entries['main']['chatbots'] = "Chatbots";
-dict.entries['main']['clipboards'] = "Zwischenablage";
+dict.entries['main']['clipboards'] = "Zwischenablage (Clipboard)";
 dict.entries['main']['devices'] = "Geräte";
 dict.entries['main']['display'] = "Display-Manager";
 dict.entries['main']['icons'] = "Icons";
@@ -46,9 +46,9 @@ dict.entries['main']['filemanagers'] = "Dateimanager";
 dict.entries['main']['monitor'] = "Überwachung";
 dict.entries['main']['term'] = "Terminals";
 dict.entries['main']['themes'] = "Themes";
-dict.entries['main']['translators'] = "Übersetzungsprogramme";
+dict.entries['main']['translators'] = "Übersetzer";
 dict.entries['main']['usb'] = "USB";
-dict.entries['main']['weather'] = "Wettter";
+dict.entries['main']['weather'] = "Wetter";
 // games
 dict.entries['main']['adventure'] = "Abenteuer";
 dict.entries['main']['arcade'] = "Spielhalle";
@@ -92,11 +92,11 @@ dict.entries['main']['videoplayers'] = "Video-Player";
 // office
 dict.entries['main']['addressbooks'] = "Adressbücher";
 dict.entries['main']['calculators'] = "Rechner";
-dict.entries['main']['documentviewers'] = "Dokumentbetrachter";
+dict.entries['main']['documentviewers'] = "Dokumenten- betrachter";
 dict.entries['main']['finance'] = "Finanzen";
 dict.entries['main']['notes'] = "Notizen";
 dict.entries['main']['officesuite'] = "Office-Paket";
-dict.entries['main']['spreadsheet'] = "Tabellenkalkulationen";
+dict.entries['main']['spreadsheet'] = "Tabellen- kalkulationen";
 dict.entries['main']['texteditors'] = "Texteditoren";
 // others
 dict.entries['main']['others'] = "Sonstiges";
@@ -114,13 +114,13 @@ dict.entries['main']['shells'] = "Shells";
 dict.entries['main']['virtual'] = "Virtuell";
 // search
 dict.entries['main']['search'] = "Suche";
-dict.entries['main']['info12'] = "Suchergebnis für das Wort";
-dict.entries['main']['info13'] = "Sorry, kann leider NICHT finden, wonach Sie suchen.";
+dict.entries['main']['info12'] = "Suchergebnis für das Wort:";
+dict.entries['main']['info13'] = "Sorry, kann NICHT finden, wonach Sie suchen.";
 dict.entries['main']['info14'] = "Frischen Sie die Paketliste auf und versuchen Sie es erneut unter Verwendung eines spezifischeren Paketnamens.";
 dict.entries['main']['info15'] = "Zurück zu:";
 dict.entries['main']['info16'] = "Der Hauptseite.";
 dict.entries['main']['info17'] = "Suchergebnis für das Wort:";
 dict.entries['main']['info18'] = "Es gibt mehr als eines Ihrer Suchergebnisse.";
 dict.entries['main']['info19'] = "Bitte wählen Sie eine Seite, welche besser zu Ihrem Schlüsselwort passt.";
-dict.entries['main']['info20'] = "ist mehr als eine.";
+dict.entries['main']['info20'] = "ist mehr als eines.";
 dict.entries['main']['info21'] = "NULL";
