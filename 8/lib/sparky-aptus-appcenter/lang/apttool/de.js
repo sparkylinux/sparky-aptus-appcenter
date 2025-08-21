82734ac82877ce2f1db9e76330ec148c959ab572
pavroo <pavroo@onet.eu>
2025-08-21 19:55:22 +0200

    20250821

--- 8/lib/sparky-aptus-appcenter/lang/apttool/de.js
+++ 8/lib/sparky-aptus-appcenter/lang/apttool/de.js
@@ -1,4 +1,4 @@
-// German localization file for APTus Tools (03.06.2025)
+// German localization file for APTus Tools (17.08.2025)
 // Copyright 2020 Pawel "pavroo" Pijanowski <pavroo@onet.eu>
 // Under the GNU GPL v3
 // Last update 2020/10/20 by pavroo
@@ -6,10 +6,10 @@ dict.entries['apttool'] = {};
 // aptus tools
 dict.entries['apttool']['refresh'] = "Paketliste auffrischen";
 dict.entries['apttool']['upgrade'] = "System sicher upgraden";
-dict.entries['apttool']['fullupgrade'] = "Vollstaendiges System-Upgrade";
+dict.entries['apttool']['fullupgrade'] = "Vollständiges System-Upgrade";
 dict.entries['apttool']['installdebrepo'] = "Pakete aus Repository installieren";
 dict.entries['apttool']['installdeblocal'] = "Paket von lokalem Laufwerk installieren";
-dict.entries['apttool']['fixpackages'] = "Beschaedigte Pakete reparieren";
+dict.entries['apttool']['fixpackages'] = "Beschädigte Pakete reparieren";
 dict.entries['apttool']['aptclean'] = "Alle DEB-gecachten Pakete bereinigen";
 dict.entries['apttool']['aptautoremove'] = "Alte Pakete autom. entfernen";
 dict.entries['apttool']['aptremove'] = "Paket aus System entfernen";
